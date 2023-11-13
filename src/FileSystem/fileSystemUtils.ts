3a95ebb1bd71d7c70468a0480fd80780656c828d
Mateusz Duda <mateusz.duda2@gmail.com>
2023-11-13 22:10:07 +0100

    added tests for fileSystemUtils

--- src/FileSystem/fileSystemUtils.ts
+++ src/FileSystem/fileSystemUtils.ts
@@ -14,16 +14,3 @@ export function ensureScopeFolderExists(root: string): string {
 	}
 	return scopeFolderPath;
 }
-
-export function findTsConfigRecursively(): string { // Unused
-	let currentDir = __dirname;
-
-	while (!fs.existsSync(path.join(currentDir, "tsconfig.json"))) {
-		currentDir = path.join(currentDir, "..");
-
-		if (path.resolve(currentDir) === "/") {
-			throw new Error("Could not locate tsconfig.json");
-		}
-	}
-	return currentDir;
-}
