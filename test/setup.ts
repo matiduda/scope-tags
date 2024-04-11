92b8c1e540e6d6fb2d435d809368e00c630939b8
Mateusz Duda <mateusz.duda@beefee.co.uk>
2024-04-11 11:31:54 +0200

    added path resolve in test init

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,9 @@
 module.exports = async function(globalConfig, projectConfig) {
     const rimraf = require("rimraf");
     const fs = require("fs");
+    const path = require("path");
 
-    global.TEMP_FOLDER_FOR_TESTS = "./TEST_TMP/";
+    global.TEMP_FOLDER_FOR_TESTS = path.resolve("./TEST_TMP/");
 
     console.debug(`\n[SCOPE TAGS] Global test setup - initializing temporary folder at ${global.TEMP_FOLDER_FOR_TESTS}`);
     rimraf.sync(global.TEMP_FOLDER_FOR_TESTS);
