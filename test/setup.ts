31a1a46363f668a9464fb28b4d1fd4960785e57a
Mateusz Duda <mateusz.duda@beefee.co.uk>
2024-04-11 11:54:59 +0200

    updated test

--- test/setup.ts
+++ test/setup.ts
@@ -6,7 +6,11 @@ module.exports = async function(globalConfig, projectConfig) {
     global.TEMP_FOLDER_FOR_TESTS = path.resolve("./TEST_TMP/");
 
     console.debug(`\n[SCOPE TAGS] Global test setup - initializing temporary folder at ${global.TEMP_FOLDER_FOR_TESTS}`);
-    rimraf.sync(global.TEMP_FOLDER_FOR_TESTS);
+
+    if (fs.existsSync(global.TEMP_FOLDER_FOR_TESTS)) {
+        console.debug(`\n[SCOPE TAGS] Removing previous instance of '${global.TEMP_FOLDER_FOR_TESTS}'`);
+        rimraf.sync(global.TEMP_FOLDER_FOR_TESTS);
+    }
 
     try {
         fs.mkdirSync(global.TEMP_FOLDER_FOR_TESTS, { recursive: true });
