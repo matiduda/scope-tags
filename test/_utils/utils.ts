064cf3c9d86b1fe58030299216068f2d44348754
Mateusz Duda <mateusz.duda@beefee.co.uk>
2024-04-11 14:03:59 +0200

    updated tets

--- test/_utils/utils.ts
+++ test/_utils/utils.ts
@@ -1,5 +1,5 @@
 import { resolve, join } from "path";
-import { existsSync } from "fs";
+import { existsSync, mkdirSync } from "fs";
 import { copySync } from "fs-extra";
 
 import rimraf from "rimraf";
@@ -36,15 +36,6 @@ export const purgeMockRepository = () => {
 export const assertTemporaryFolderExists = () => {
     const folderPath = resolve(TEMP_TEST_FOLDER);
     if (!existsSync(folderPath)) {
-        // For debug - print info
-        const { exec } = require('child_process');
-        exec('pwd && ls -ltra', (err: any, stdout: any, stderr: any) => {
-            // the *entire* stdout and stderr (buffered)
-            console.debug(`stdout: ${stdout}`);
-        });
-
-
-
-        throw new Error(`Temporary folder '${folderPath}' does not exist`);
+        mkdirSync(folderPath);
     }
 }
